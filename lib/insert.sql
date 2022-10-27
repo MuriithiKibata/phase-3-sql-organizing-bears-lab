08e3de9541c995d7cb0e26aee531ab149a91238c
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-10-27 16:40:39 +0300

    DONE

---
+++ lib/insert.sql
@@ -0,0 +1,27 @@
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Beorn", 12, "M", "brown", "volotile", "yes");
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Ham", 52, "M", "red", "volotile", "yes");
+ INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+ VALUES("Hammer", 32, "M", "brown", "volotile", "yes");
+
+ INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Hand", 32, "M", "brown", "volotile", "yes");
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+ VALUES(NULL, 21, "M", "brown", "volotile", "yes");
+
+ INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+ VALUES("Warwick", 90, "M", "grey", "extremely volotile", "yes");
+ INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+ VALUES("Joy", 13, "F", "black", "kind", "yes");
+
+ INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+ VALUES("Erik", 15, "M", "brown", "gentle", "yes");
